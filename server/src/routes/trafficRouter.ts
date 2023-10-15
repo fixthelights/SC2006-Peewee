e82ffa91b3efbe4f0004ec0699c1b0fdb704a325
Lim Zheng Guang <40014625+fixthelights@users.noreply.github.com>
2023-10-16 03:28:39 +0800

    Added traffic endpoints and documentation

--- server/src/routes/trafficRouter.ts
+++ server/src/routes/trafficRouter.ts
@@ -2,26 +2,23 @@ import express, { Request, Response } from 'express';
 const router = express.Router();
 const trafficController = require('../controllers/trafficController');
 
-// GET request for a list of car counts for each camera
+// GET request for a list of latest consolidated vehicle counts across all cameras
+router.get('/combined-conditions/', trafficController.getConsolidatedTrafficConditions);
+
+// GET request for a list of latest vehicle counts for all cameras
 router.get('/conditions/', trafficController.getTrafficConditionsAllCameras);
 
-// GET request for a list of car counts for one camera
+// GET request for a list of latest vehicle counts for one camera
 router.get('/conditions/:cameraId', trafficController.getTrafficConditionsOneCamera);
 
-// GET request for a collated list of car counts across all cameras
-//router.get('/conditions/collated', trafficController);
+
+// GET request for consolidated traffic trends across all cameras
+router.get('/combined-trends/', trafficController.getConsolidatedTrends);
+
+// GET request for traffic trends on one camera
+router.get('/trends/:cameraId', trafficController.getTrendsOneCamera);
 
 // PUT request to generate trends on the current day, and overwrite existing trends
 router.put('/trends/', trafficController.generateTrends);
-
-// GET request for traffic trends on all cameras
-router.get('/trends/', trafficController.getTrendsAllCameras);
-
-// GET request for traffic trends on one camera
-//router.get('/trends/:cameraId', trafficController);
-
-// GET request for collated traffic trends across all cameras
-//router.get('/collated-trends', trafficController);
-
 
 module.exports = router;
