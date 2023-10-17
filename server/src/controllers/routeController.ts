c7fc83205015ddad33b69571d7d91ae461d136fa
sampsamp101 <aaronjiwei@gmail.com>
2023-10-17 21:35:27 +0800

    did changes to routeController.ts

--- server/src/controllers/routeController.ts
+++ server/src/controllers/routeController.ts
@@ -1,52 +1,81 @@
-import { Request, Response } from 'express';
-import Route from '../models/route';
+import express, { Request, Response } from 'express';
+import{AppError} from "../config/AppError";
+import Route from "../models/route";
 
-
-exports.createRoutes = async(req: Request, res: Response)=>{
+exports.createRoutes = async (req: Request, res: Response) => {
     const route = new Route(req.body);
-    try{
+    try {
         const savedRoute = await route.save();
-        return res.status(201).send(savedRoute);
-    } catch(err: any){
-        return res.status(400).send(err.message);
+        res.status(201).send(savedRoute);
+    } catch (err: any) {
+        throw new AppError({
+            error: err,
+            statusCode: 400,
+            description: err.message
+        });
     }
+};
+
+exports.findRoutes = async (req: Request, res: Response) => {
+    try {
+        const routes = await Route.find();
+        res.send(routes);
+    } catch (err: any) {
+        throw new AppError({
+            error: err,
+            statusCode: 500,
+            description: "Error fetching routes!"
+        });
     }
+};
 
-exports.findRoutes = async (req:Request, res: Response)=>{
-        try {
-           const routes = await Route.find();
-           return res.send(routes);
-        } catch(err: any){
-           return res.status(500).send(err.message);
-        }
-       }
-    
-exports.getRoutes =  async(req:Request,res:Response)=>{
-        try {
-          const route = await Route.findById(req.params.id);
-          if(!route) return res.status(404).send('Route not found');
-          return res.send(route);
-        }catch(err : any){
-          return res.status(500).send(err.message);
-        }
-      }
-
-exports.updateRoutes = async(req: Request, res: Response) => {
-        try {
-            const route = await Route.findByIdAndUpdate(req.params.id, req.body, { new: true });
-            if (!route) return res.status(404).send('Route not found');
-            return res.status(200).send(route);
-        } catch(err : any) {
-            return res.status(400).send(err.message);
-        }
-    };
-
-exports.deleteRoutes = async (req:Request, res:Response) => {
-        try {
-            const route = await Route.findByIdAndDelete(req.params.id);
-            if (!route) return res.status(404).send('Route not found');
-            return res.status(204).send();
-        } catch (err : any) {
-            return res.status(500).send(err.message);
-        }
+export const getRoutes = async (req: Request, res: Response) => {
+    try {
+        const route = await Route.findById(req.params.id);
+        if (!route) throw new AppError({
+            statusCode: 404,
+            description: 'Route not found'
+        });
+        res.send(route);
+    } catch (err: any) {
+        throw new AppError({
+            error: err,
+            statusCode: 500,
+            description: "Error fetching the route!"
+        });
     }
+};
+
+export const updateRoutes = async (req: Request, res: Response) => {
+    try {
+        const route = await Route.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!route) throw new AppError({
+            statusCode: 404,
+            description: 'Route not found'
+        });
+        res.status(200).send(route);
+    } catch (err: any) {
+        throw new AppError({
+            error: err,
+            statusCode: 400,
+            description: "Error updating the route!"
+        });
+    }
+};
+
+export const deleteRoutes = async (req: Request, res: Response) => {
+    try {
+        const route = await Route.findByIdAndDelete(req.params.id);
+        if (!route) throw new AppError({
+            statusCode: 404,
+            description: 'Route not found'
+        });
+        res.status(204).send();
+    } catch (err: any) {
+        throw new AppError({
+            error: err,
+            statusCode: 500,
+            description: "Error deleting the route!"
+        });
+    }
+};
